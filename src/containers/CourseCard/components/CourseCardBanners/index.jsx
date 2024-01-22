36de690b9a8c78753766cf5d747a2ce7ea389830
Bencyu <bencyu@ust.hk>
2024-01-22 06:04:57 +0000

    Remove certificate and credit banner

--- src/containers/CourseCard/components/CourseCardBanners/index.jsx
+++ src/containers/CourseCard/components/CourseCardBanners/index.jsx
@@ -4,8 +4,6 @@ import PropTypes from 'prop-types';
 import { reduxHooks } from 'hooks';
 
 import CourseBanner from './CourseBanner';
-import CertificateBanner from './CertificateBanner';
-import CreditBanner from './CreditBanner';
 import EntitlementBanner from './EntitlementBanner';
 import RelatedProgramsBanner from './RelatedProgramsBanner';
 
@@ -16,8 +14,6 @@ export const CourseCardBanners = ({ cardId }) => {
       <RelatedProgramsBanner cardId={cardId} />
       <CourseBanner cardId={cardId} />
       <EntitlementBanner cardId={cardId} />
-      {isEnrolled && <CertificateBanner cardId={cardId} />}
-      {isEnrolled && <CreditBanner cardId={cardId} />}
     </div>
   );
 };
