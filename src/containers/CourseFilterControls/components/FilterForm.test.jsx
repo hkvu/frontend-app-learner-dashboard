b637e6d1d8323fb1c640a8291d7f30810270dcf5
Bencyu <bencyu@ust.hk>
2024-01-23 02:11:16 +0000

    Remove notEnrolled and upgraded filters

--- src/containers/CourseFilterControls/components/FilterForm.test.jsx
+++ src/containers/CourseFilterControls/components/FilterForm.test.jsx
@@ -22,8 +22,6 @@ describe('FilterForm', () => {
       FilterKeys.inProgress,
       FilterKeys.notStarted,
       FilterKeys.done,
-      FilterKeys.notEnrolled,
-      FilterKeys.upgraded,
     ]);
   });
 });
