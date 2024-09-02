3e4eb1bb8fa95b92e9627431a301da12ca5d5942
Bencyu <bencyu@ust.hk>
2024-09-02 15:34:53 +0800

    Add maintenance messgae.

--- src/containers/LearnerDashboardHeader/index.jsx
+++ src/containers/LearnerDashboardHeader/index.jsx
@@ -5,11 +5,15 @@ import ConfirmEmailBanner from './ConfirmEmailBanner';
 
 import CollapsedHeader from './CollapsedHeader';
 import ExpandedHeader from './ExpandedHeader';
+import { Alert } from '@edx/paragon';
 
 import './index.scss';
 
 export const LearnerDashboardHeader = () => (
   <>
+    <Alert variant="danger">
+      Please be aware that we will be performing server maintenance on 19th September 2024 from 10:00am (HKT) to 4:00pm (HKT). HKMOOC will be unavailable during the maintenance window. Contact us at hkvu@ust.hk if you have any questions.
+    </Alert>
     <ConfirmEmailBanner />
     <CollapsedHeader />
     <ExpandedHeader />
