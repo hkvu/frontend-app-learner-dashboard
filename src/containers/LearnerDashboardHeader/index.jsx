df9ad6f346373f093a05666a224430a56b90a16b
Bencyu <bencyu@ust.hk>
2024-09-02 16:40:26 +0800

    Update maintenance style

--- src/containers/LearnerDashboardHeader/index.jsx
+++ src/containers/LearnerDashboardHeader/index.jsx
@@ -11,8 +11,11 @@ import './index.scss';
 
 export const LearnerDashboardHeader = () => (
   <>
-    <Alert variant="danger">
-      Please be aware that we will be performing server maintenance on 19th September 2024 from 10:00am (HKT) to 4:00pm (HKT). HKMOOC will be unavailable during the maintenance window. Contact us at hkvu@ust.hk if you have any questions.
+    <Alert variant="danger" dismissible icon={Info}>
+      <Alert.Heading>System Maintenance</Alert.Heading>
+      <p>
+        Please be aware that we will be performing server maintenance on 19th September 2024 from 10:00am (HKT) to 4:00pm (HKT). HKMOOC will be unavailable during the maintenance window. Contact us at hkvu@ust.hk if you have any questions.
+      </p>
     </Alert>
     <ConfirmEmailBanner />
     <CollapsedHeader />
